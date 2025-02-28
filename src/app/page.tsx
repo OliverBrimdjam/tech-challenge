3333f36ed9d35d8bee4b68adbf72be26c26843f4
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-28 07:07:33 -0300

    [feat]: set ProductList sketch on main page;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
+import { TProduct } from "@/@types/TProduct";
 import CategoryList from "@/components/molecules/CategoryList/CategoryList";
+import ProductsList from "@/components/molecules/ProductList/ProductList";
 
 export default async function Home() {
   const categories = await fetchCategories();
+  const products: TProduct[] = await fetchAllProducts();
 
   return (
     <>
       <header className="bg-amber-400 h-16">olá</header>
       <main className="flex flex-row">
         <CategoryList categories={categories} />
-        <section aria-labelledby="products-list">
-          <h2 id="products-list">Products List</h2>
-        </section>
+        <ProductsList products={products} />
       </main>
       <footer>footer</footer>
     </>
@@ -19,5 +20,10 @@ export default async function Home() {
 // ----------------------------------------------------------------
 async function fetchCategories() {
   const response = await fetch('https://fakestoreapi.com/products/categories');
+  return response.json();
+}
+
+async function fetchAllProducts() {
+  const response = await fetch('https://fakestoreapi.com/products');
   return response.json();
 }
