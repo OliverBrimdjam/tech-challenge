1dca44f4a3ce398078730c3035bffb80c2ad73dd
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-27 22:15:14 -0300

    [feat]: tdd cycle to set layout;

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,11 +1,17 @@
 import Home from "./page";
 import { render, screen } from '@testing-library/react';
 
-describe('MyComponent', () => {
-  it('renders correctly', () => {
+describe('Home Page (products page): ', () => {
+  it('renders correctly: ', () => {
     render(<Home />);
-    const component = screen.getByText('hello world!!');
+    const header = screen.getByRole('header');
+    const footer = screen.getByRole('footer');
+    const main = screen.getByRole('main');
+    const section = screen.getByRole('section');
 
-    expect(component).toBeInTheDocument();
+    expect(header).toBeInTheDocument();
+    expect(footer).toBeInTheDocument();
+    expect(main).toBeInTheDocument();
+    expect(section).toBeInTheDocument();
   });
 });
