bbb93d706192375c0c79c921b144860173a07592
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-27 21:16:31 -0300

    [chore]: automated test config;

---
+++ src/app/page.test.tsx
@@ -0,0 +1,11 @@
+import Home from "./page";
+import { render, screen } from '@testing-library/react';
+
+describe('MyComponent', () => {
+  it('renders correctly', () => {
+    render(<Home />);
+    const component = screen.getByText('hello world!');
+
+    expect(component).toBeInTheDocument();
+  });
+});
