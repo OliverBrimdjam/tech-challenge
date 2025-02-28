30dc975dce8060bc2565f5b025d4cca774717536
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-28 12:17:58 -0300

    [feat]: implement order by price;

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -6,30 +6,57 @@ import React, { useState, useEffect, useContext, createContext } from "react";
 type TProductContextType = {
   products: TProduct[];
   setCategory: (category: string | null) => void;
+  setIsOrdered: () => void;
+  isOrdered: boolean;
 }
 
 type TProductProviderProps = { children: React.ReactNode; startProducts: TProduct[] }
 
-const ProductContext = createContext<TProductContextType>({ products: [], setCategory: () => {} });
+const ProductContext = createContext<TProductContextType>({ 
+  products: [], 
+  setCategory: () => {}, 
+  setIsOrdered: () => {},
+  isOrdered: false 
+});
 
 export function ProductProvider ({ children, startProducts }: TProductProviderProps) {
   const [products, setProducts] = useState<TProduct[]>(startProducts);
   const [category, setCategory] = useState<string | null>(null);
+  const [isOrdered, setIsOrdered] = useState<boolean>(false);
 
   const fetchProducts = async function (category: string | null) {
     const response = await fetch(
       category ? `https://fakestoreapi.com/products/category/${category}` : 'https://fakestoreapi.com/products'
     );
     const data = await response.json();
-    setProducts(data);
+    setProducts(isOrdered ? sortProductsByPrice(data) : data);
+  }
+
+  const sortProductsByPrice = (products: TProduct[]) => {
+    return [...products].sort((a, b) => a.price - b.price);
+  }
+
+  const toggleOrder = () => {
+    setIsOrdered(prev => !prev);
+    setProducts(prev => isOrdered ? prev : sortProductsByPrice(prev));
+    console.log('click')
   }
 
   useEffect(() => {
     fetchProducts(category);
   },[category]);
 
+  useEffect(() => {
+    setProducts(prev => isOrdered ? sortProductsByPrice(prev) : [...prev].sort((a, b) => a.id - b.id));
+  }, [isOrdered]);
+
   return (
-    <ProductContext.Provider value={{ products, setCategory }}>
+    <ProductContext.Provider value={{ 
+      products, 
+      setCategory, 
+      setIsOrdered: toggleOrder,
+      isOrdered 
+    }}>
       {children}
     </ProductContext.Provider>
   );
