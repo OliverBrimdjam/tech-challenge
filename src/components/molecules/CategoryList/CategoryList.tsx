969a11bd60bd321d325d17df44be832aa713f4d9
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-28 10:58:27 -0300

    [feat]: implement category filter handle by context;

--- src/components/molecules/CategoryList/CategoryList.tsx
+++ src/components/molecules/CategoryList/CategoryList.tsx
@@ -1,17 +1,23 @@
+'use client'
+
 import Button from "@/components/atoms/Button/Button";
+import { useProducts } from "@/contexts/ProductContext";
 
 type TCategoryListProps = {
   categories: string[];
 }
 
 export default function CategoryList({categories}: TCategoryListProps) {
+  const {setCategory} = useProducts()
+
   return (
     <section aria-labelledby="categories">
       <nav className="flex flex-col w-[320px]">
         <h2 className="text-3xl m-2" id="categories">Categories</h2>
+        <Button className="m-2" onClick={() => setCategory(null)}>All</Button>
         {categories.map((category)=>{
           return(
-            <Button className="m-2" key={category}>{category}</Button>
+            <Button className="m-2" onClick={() => setCategory(category)} key={category}>{category}</Button>
           );
         })}
       </nav>
