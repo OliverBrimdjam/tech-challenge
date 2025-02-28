98bd16a9ef83835cb2a5b62e7281746e569ce5f7
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2025-02-28 16:17:58 -0300

    [feat]: set the ProductForm on ProductList component;

--- src/components/molecules/ProductList/ProductList.tsx
+++ src/components/molecules/ProductList/ProductList.tsx
@@ -4,6 +4,7 @@ import Button from "@/components/atoms/Button/Button";
 import ProductCard from "../ProductCard/ProductCard";
 import { useProducts } from "@/contexts/ProductContext";
 import Pagination from "@/components/atoms/Pagination/Pagination";
+import { ProductForm } from "../ProductForm/ProductForm";
 
 export default function ProductsList() {
   const { products, toggleOrder, setCurrentPage } = useProducts();
@@ -12,7 +13,10 @@ export default function ProductsList() {
     <section aria-labelledby="products-list">
       <div className="flex justify-between flex-wrap">
         <h2 id="products-list" className="text-3xl m-2">Product List</h2>
-        <Button className="my-auto mx-6" onClick={toggleOrder}>Order By Price</Button>
+        <div className="flex mx-6 my-auto gap-6">
+          <Button onClick={toggleOrder}>Order By Price</Button>
+          <ProductForm />
+        </div>
       </div>
       <div className="flex-1 flex flex-wrap">
         {products.data.map((product) => (
